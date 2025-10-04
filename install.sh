c3cf7584a3c2b2559d9ce369eed077b73f0c81e5
entish84 <entishthoughts@outlook.com>
2025-10-04 13:04:15 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,7 +20,23 @@ sudo apt install fonts-inter fonts-jetbrains-mono fonts-cascadia-code
 # Install jdk (17 because of MAUI otherwise latest)
 sudo apt install openjdk-17-jdk -y
 
+# Install zsh and oh-my-zsh
+sudo apt install zsh -y
+zsh --version
+chsh -s $(which zsh)
+echo $SHELL
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+#### 1. **Zsh Autosuggestions**
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+#### 2. **Zsh Syntax Highlighting**
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+#### 3. **Zsh Autocomplete**
+git clone --depth 1 https://github.com/marlonrichert/zsh-autocomplete ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete
+#### 4. **Zsh History Substring Search (optional)**
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 
-
+nano ~/.zshrc
+plugins=(sudo themes aliases ubuntu git zsh-autosuggestions zsh-syntax-highlighting zsh-autocomplete z zsh-history-substring-search)
+source ~/.zshrc
 
