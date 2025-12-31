fb648a3588512a18b1aa68100965271818e290f4
entish84 <entishthoughts@outlook.com>
2025-12-31 16:46:40 +0530

    Add installation for zoxide, fzf, and eza
    Added installation steps for zoxide, fzf, and eza in install.sh.

--- install.sh
+++ install.sh
@@ -33,6 +33,23 @@ starship preset gruvbox-rainbow -o ~/.config/starship.toml
 # Configure Powerline preset
 starship preset catppuccin-powerline -o ~/.config/starship.toml
 
+# install zoxide fzf and eza
+sudo apt install zoxide
+
+# FZF
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+#EZA
+sudo mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+sudo apt update
+sudo apt install -y eza
+
+# Add Aliases and entries in bashrc and source
+
 # Install jdk (17 because of MAUI otherwise latest)
 sudo apt install openjdk-21-jdk -y
 
