4efc0bc839a7c69952e42b13995734c24d86cbe9
entish84 <entishthoughts@outlook.com>
2026-02-15 12:41:23 +0530

    Update install script to include new packages and repos
    Added installation of additional packages and enabled new COPR repositories for fonts and utilities.

--- FedoraPI/install.sh
+++ FedoraPI/install.sh
@@ -1,6 +1,9 @@
+sudo dnf install gcc make cmake clang cargo meld p7zip tar ninja-build
+
 sudo dnf copr enable lihaohong/yazi
 sudo dnf copr enable jakjasie1/timeshift
 sudo dnf copr enable alternateved/eza
+sudo dnf copr enable atim/bottom -y
 
 sudo dnf install zoxide eza fzf bat yazi -y
 
@@ -9,16 +12,33 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 # Nerd font installer
 # https://github.com/LionyxML/nerd-installer
 
-# curl -fsSL https://raw.githubusercontent.com/getnf/getnf/main/install.sh | bash
+sudo dnf copr enable julicen/atkinson-hyperlegible-fonts -y
+# 2. Enable the reliable repo for Inter and JetBrains
+sudo dnf copr enable elxreno/jetbrains-mono-fonts -y
+# 3. Install with the exact current package names
+sudo dnf install -y \
+    atkinson-hyperlegible-next-fonts \
+    atkinson-hyperlegible-mono-fonts \
+    rsms-inter-vf-fonts \
+    google-roboto-fonts \
+    jetbrains-mono-fonts-all
+# 4. Refresh font cache
+fc-cache -fv
 
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 sudo dnf upgrade --refresh
 sudo dnf install code
  
-sudo dnf install kitty foliate
-
+sudo dnf install kitty foliate micro haruna
 sudo dnf install timeshift
+sudo dnf install bottom -y
+
+mkdir -p ~/.config/mpv/
+nano ~/.config/mpv/mpv.conf
+#Add this line: hwdec=auto-safe
+
+
 
 sudo dnf copr enable lukenukem/asus-linux
 sudo dnf install asusctl supergfxctl
